0eada69327d4f4e69d4a7d94b1cb528159450a12
José Torres <torresv.josemiguel@gmail.com>
2024-09-24 01:02:24 +0200

    Xcode Project Updated

--- flying app/ContentView.swift
+++ flying app/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab = 0
+    
     var body: some View {
-        VStack {
-            Text("Flying App")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            Image("airport_pexels")
-                .resizable()
-                .cornerRadius(15)
-                .aspectRatio(contentMode: .fit)
-                .padding(.all)
+        TabView {
+            
+            VStack {
+                Text("Flying App")
+                    .font(.title)
+                List {
+                    PersonCardView(person: person1)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person2)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person1)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person2)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person1)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person2)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person1)
+                        .listRowSeparator(.hidden)
+                    PersonCardView(person: person2)
+                        .listRowSeparator(.hidden)
+                }
+                .listStyle(.plain)
+            }
+            .tabItem {
+                Image(systemName: "plus")
+                Text("Home")
+            }
+            .tag(0)
+            
+            Text("Hi")
+                .tabItem {
+                    Image(systemName: "pencil")
+                    Text("Profile")
+                }
+                .tag(1)
+            
+            Text("Hi mundo")
+                .tabItem {
+                    Image(systemName: "circle")
+                    Text("Profile")
+                }
+                .tag(2)
         }
     }
 }
---
+++ flying app/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  flying app
+//
+//  Created by José Torres on 23/09/24.
+//
+
+import Foundation
+
+struct Person {
+    let headerImage: String
+    let profileImage: String
+    let name: String
+    let followerCount: Int
+    let jobTitle: String
+}
+
+let person1 = Person(headerImage: "HeaderImage1", profileImage: "ProfileImage1", name: "José Torres", followerCount: 1000, jobTitle: "Developer")
+let person2 = Person(headerImage: "HeaderImage2", profileImage: "ProfileImage2", name: "Carlos Torres", followerCount: 2000, jobTitle: "Artist")
